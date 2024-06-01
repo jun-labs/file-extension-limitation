d65210b7fe66c0fbbfffada221a7145e46697fac
devjun10 <devjun1023@gmail.com>
2024-06-02 01:46:35 +0900

    chore: 프로젝트 빌드 - SpringBoot, Jpa/QueryDsl, Swagger, H2, Test 의존성 추가

---
+++ src/main/java/project/io/app/FileExtensionLimitationApplication.java
@@ -0,0 +1,11 @@
+package project.io.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExtensionLimitationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileExtensionLimitationApplication.class, args);
+    }
+}
